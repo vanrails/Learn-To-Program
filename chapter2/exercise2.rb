1c7ed24454b65139fc1de8fd775d6be4b20a2ee1
vanrails <rv28@waikato.ac.nz>
2013-11-04 14:20:38 +0000

    commit for chapter 2

---
+++ chapter2/exercise2.rb
@@ -0,0 +1,12 @@
+# How many hours in a year
+puts 'Hours in a year:'
+puts 365 * 24 
+
+# Number of minutes in a decade
+# excluding leap days
+puts 'Minutes in a decade (excluding leap days):'
+puts 10 * 365 * 24 * 60
+
+# Age in seconds.
+puts 'Age in seconds:'
+puts 24 * 365 * 24 * 60 * 60
